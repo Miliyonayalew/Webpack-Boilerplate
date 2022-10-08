872d5357e86b8e8b2935130013aad5085dc55300
Miliyonayalew <miliayalew@gmail.com>
2022-10-08 20:30:43 +0300

    Split dev and prod webpack configration and Implemented cache busting

---
+++ webpack.dev.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devtool: 'source-map',
+  devServer: {
+    watchFiles: path.resolve(__dirname, 'src'),
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  }
+});
